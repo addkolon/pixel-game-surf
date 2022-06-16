c337ed0c37ee234bee029f63e88099bb6683dcc0
olof <aksberg.olof@outlook.com>
2022-06-16 16:32:51 +0200

    start finalize

--- game/src/pages/game/Game.js
+++ game/src/pages/game/Game.js
@@ -1,11 +1,12 @@
 /** @format */
 
 import {
-  lives,
-  getLives,
-  gameOver,
-  setGameOver,
-  startGame,
+ lives,
+ getLives,
+ gameOver,
+ setGameOver,
+ startGame,
+ resetGameplay,
 } from "../../store/gameplaySlice";
 
 import { data, getTopScores } from "../../store/scoresSlice";
@@ -19,77 +20,81 @@ import { useNavigate } from "react-router-dom";
 import { settings } from "./settings";
 import { Scoreboard } from "../../components/Scoreboard";
 import { useHandleGameMusic } from "./utils/sounds/useHandleGameMusic";
-import { reset } from "../../store/playerObjectSlice";
+import { resetPlayer } from "../../store/playerObjectSlice";
+import { resetSpawners } from "../../store/spawnersSlice";
 
 export const Game = ({}) => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const gameOverState = useSelector(gameOver);
-  const scores = useSelector(data);
-  const lives = useSelector((state) => state.gameplay.lives);
-  const score = useSelector((state) => state.gameplay.score);
+ const navigate = useNavigate();
+ const dispatch = useDispatch();
+ const gameOverState = useSelector(gameOver);
+ const scores = useSelector(data);
+ const lives = useSelector((state) => state.gameplay.lives);
+ const score = useSelector((state) => state.gameplay.score);
 
-  const { play, stop, song, setSong } = useHandleGameMusic();
+ const { play, stop, song, setSong } = useHandleGameMusic();
 
-  useEffect(() => {
-    if (lives.length < 1) {
-      navigate("/");
-      dispatch(reset());
-      dispatch(setGameOver(true));
+ useEffect(() => {
+  if (lives.length < 1) {
+   navigate("/");
+   dispatch(resetSpawners());
+   dispatch(resetPlayer());
+   dispatch(resetGameplay(true));
+
+   // dispatch(setGameOver(true));
+  }
+ }, [lives]);
+
+ // useEffect(() => {
+ //   dispatch(getTopScores("page=1&limit=10"));
+ // }, []);
+
+ useEffect(() => {
+  song ? play() : stop();
+  return () => stop();
+ }, [song, play, stop]);
+
+ return (
+  <div
+   className="main"
+   onKeyDown={(e) => {
+    if (e.code === "KeyM") {
+     setSong(!song);
     }
-  }, [lives]);
-
-  // useEffect(() => {
-  //   dispatch(getTopScores("page=1&limit=10"));
-  // }, []);
-
-  useEffect(() => {
-    song ? play() : stop();
-    return () => stop();
-  }, [song, play, stop]);
-
-  return (
-    <div
-      className="main"
-      onKeyDown={(e) => {
-        if (e.code === "KeyM") {
-          setSong(!song);
-        }
-      }}
-    >
-      <main className="game-container">
-        <div className="screen"></div>
-        <div className="top">
-          <div id="lives">
-            <ul id="livesUl">
-              {lives.map((l) => {
-                return (
-                  <li>
-                    <img src={lifeRing} width="35px" />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="in-game-score">
-            <h2>
-              HIGH SCORE: <span id="high-scoure">12500</span>
-            </h2>
-            <h2>
-              YOUR SCORE: <span id="saves">{score}</span>
-            </h2>
-          </div>
-        </div>
-        <Canvas />
-        <div className="bottom">
-          <div>Key M: {song ? "mute" : "unmute"}</div>
-          <h2 className="music">&#9835; EVIG FERIE - ENESTE</h2>
-        </div>
-      </main>
-      <aside>
-        <img class="logo" src={logotype} />
-        <Scoreboard scores={scores.scores} />
-      </aside>
+   }}
+  >
+   <main className="game-container">
+    <div className="screen"></div>
+    <div className="top">
+     <div id="lives">
+      <ul id="livesUl">
+       {lives.map((l) => {
+        return (
+         <li>
+          <img src={lifeRing} width="35px" />
+         </li>
+        );
+       })}
+      </ul>
+     </div>
+     <div className="in-game-score">
+      <h2>
+       HIGH SCORE: <span id="high-scoure">12500</span>
+      </h2>
+      <h2>
+       YOUR SCORE: <span id="saves">{score}</span>
+      </h2>
+     </div>
     </div>
-  );
+    <Canvas />
+    <div className="bottom">
+     <div>Key M: {song ? "mute" : "unmute"}</div>
+     <h2 className="music">&#9835; EVIG FERIE - ENESTE</h2>
+    </div>
+   </main>
+   <aside>
+    <img class="logo" src={logotype} />
+    <Scoreboard scores={scores.scores} />
+   </aside>
+  </div>
+ );
 };
