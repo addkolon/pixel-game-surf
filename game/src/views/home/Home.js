5e13456404a2dd2d657833b636d2d23406001210
addkolon <mattias@addkolon.se>
2022-06-23 21:01:54 +0200

    Some styling and HTML on start page

--- game/src/views/home/Home.js
+++ game/src/views/home/Home.js
@@ -3,6 +3,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { data, getTopScores, status } from "../../store/scoresSlice";
+import arrowKeys from "../../sprite/arrow-keys.png";
 
 export const Home = ({ setView }) => {
  const dispatch = useDispatch();
@@ -23,7 +24,8 @@ export const Home = ({ setView }) => {
 
     <div id="content">
      <div className="score-to-beat">
-      skår to bit: {scoreToBeat.score} av {scoreToBeat.name}
+      <h1>SCORE TO BEAT: {scoreToBeat.score}</h1>
+      {/* av {scoreToBeat.name} */}
      </div>
 
      <button
@@ -34,6 +36,24 @@ export const Home = ({ setView }) => {
      >
       Start Game
      </button>
+     <div className="how-to-play">
+      <a href="#">HOW TO PLAY</a>
+     </div>
+     <div className="game-dev">
+      Created by: Olof Aksberg & Mattias Lager
+     </div>
+     <div id="modal-how-to-play">
+      <h2>HOW TO PLAY</h2>
+      <p>
+        The aim of the game is to surf your way to clean the ocean from all the trash all the brash people been throwing into it. 
+        But beware of the stones, they will brake your board.
+      </p>
+      <img src={arrowKeys} alt="" />
+      <p>
+        Use arrow keys to stear the surfer
+      </p>
+      <a href="#">CLOSE</a>
+     </div>
     </div>
    </div>
   </main>
